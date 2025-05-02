6919d389308efc488a6a736500a48e158ec00613
Jamshidbek Shodibek <159956769+Jamshidbekpy@users.noreply.github.com>
2025-05-03 02:49:37 +0500

    changed

--- core/urls.py
+++ core/urls.py
@@ -30,3 +30,31 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
+# Documentation
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="YouTube API",
+        default_version='v1',
+        description="API lar",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="jamshidbekshodibekov2004@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
