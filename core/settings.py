b3cbe268ffb30b0bc0823d3a228e7356a7425833
Jamshidbek Shodibek <159956769+Jamshidbekpy@users.noreply.github.com>
2025-05-03 02:20:48 +0500

    changed

--- core/settings.py
+++ core/settings.py
@@ -99,9 +99,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'youtube_clone_yy9h',
+        'USER': 'postgresql',
+        'PASSWORD': 'iphDBBlandPwnkH6gIk4R9Jg9E3qiujy',
+        'HOST': 'dpg-d0ajcg9r0fns739v9l0g-a',
+        'PORT': 5432,
     }
 }
 
